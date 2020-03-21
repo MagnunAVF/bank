e9e71166787ae60c76156af644ba2f3d28657b63
Magnun A V F <magnunavf@gmail.com>
2020-03-21 16:54:47 -0300

    feat: Create Transfer operation in CheckingAccount

--- accounts/checkingAccount.go
+++ accounts/checkingAccount.go
@@ -41,3 +41,22 @@ func (a *CheckingAccount) Deposit(value float64) (float64, error) {
 
 	return a.Balance, nil
 }
+
+// Transfer a value between accounts
+func (a *CheckingAccount) Transfer(value float64, destinyAccount *CheckingAccount) error {
+	validValue := value > 0
+	enoughtBalance := value <= a.Balance
+
+	if !validValue {
+		return errors.New("Invalid value in Withdraw")
+	}
+
+	if !enoughtBalance {
+		return errors.New("Not enough funds in account")
+	}
+
+	a.Balance -= value
+	destinyAccount.Deposit(value)
+
+	return nil
+}
