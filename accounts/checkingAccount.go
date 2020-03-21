87e899e91d4d363627bfba01e26195c7a88efe83
Magnun A V F <magnunavf@gmail.com>
2020-03-21 16:29:48 -0300

    feat: Create Withdraw operation in CheckingAccount

--- accounts/checkingAccount.go
+++ accounts/checkingAccount.go
@@ -1,9 +1,32 @@
 package accounts
 
-import "github.com/MagnunAVF/bank/clients"
+import (
+	"errors"
 
+	"github.com/MagnunAVF/bank/clients"
+)
+
+// CheckingAccount data
 type CheckingAccount struct {
 	Holder          clients.Client
 	Agency, Account int
-	balance         float64
+	Balance         float64
+}
+
+// Withdraw operation
+func (a *CheckingAccount) Withdraw(value float64) (float64, error) {
+	validValue := value > 0
+	enoughtBalance := value <= a.Balance
+
+	if !validValue {
+		return 0., errors.New("Invalid value in Withdraw")
+	}
+
+	if !enoughtBalance {
+		return 0., errors.New("Not enough funds in account")
+	}
+
+	a.Balance -= value
+
+	return a.Balance, nil
 }
